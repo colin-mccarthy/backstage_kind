f6d4375edd9dc080e51090228ba4bb870dc1c6ba
Colin McCarthy <colin-mccarthy@users.noreply.github.com>
2024-01-10 23:00:15 -0500

    Create backstage_kind.sh

---
+++ backstage_kind.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#
+#
+#
+
+set -oe errexit
+
+# desired cluster name; default is "kind"
+KIND_CLUSTER_NAME="backstage"
+
+
+echo "> initializing Kind cluster: ${KIND_CLUSTER_NAME}"
+
+# create a cluster
+cat <<EOF |  KIND_EXPERIMENTAL_PROVIDER=podman kind create cluster  --image kindest/node:v1.23.13 --name "${KIND_CLUSTER_NAME}" --config=-
+kind: Cluster
+apiVersion: kind.x-k8s.io/v1alpha4
+nodes:
+- role: control-plane
+EOF
